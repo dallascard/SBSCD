d5acbdc706b0c0ce3e075424bbfc3a0699ea4ba3
Dallas Card <dallas.card@gmail.com>
2023-09-07 19:47:35 -0400

    removing unnecessary import

--- general/get_substitutes.py
+++ general/get_substitutes.py
@@ -6,7 +6,6 @@ from optparse import OptionParser
 
 import torch
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 from transformers import BertModel, BertTokenizerFast, BertForMaskedLM
 from transformers.models.bert.modeling_bert import BertOnlyMLMHead
